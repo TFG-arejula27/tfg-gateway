2265c2fc8645fb482712355924785db0678da5cc
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-05-25 19:28:13 +0200

    todo

--- src/internal/gateway/handler.go
+++ src/internal/gateway/handler.go
@@ -5,11 +5,13 @@ import "github.com/gin-gonic/gin"
 func NewHandler() *gin.Engine {
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/pymemo", handlerPymemo)
 	return r
 
+}
+
+func handlerPymemo(c *gin.Context) {
+	//call pymemo
+
+	//calculate time
 }
